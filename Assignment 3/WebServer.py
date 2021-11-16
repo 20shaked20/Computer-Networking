3f15301a0b38bb2238815bdff84a088bee1624aa
Shaked <shakedlevi32@gmail.com>
2021-11-16 12:29:51 +0200

    Simple Working Hello World WebServer

---
+++ Assignment 3/WebServer.py
@@ -0,0 +1,40 @@
+# import socket module
+from socket import *
+import sys  # In order to terminate the program
+
+serverSocket = socket(AF_INET, SOCK_STREAM)
+
+serverPort = 12000  # server port, basic one
+SERVER_ADDRESS = ('127.0.0.1', serverPort)  # creating server address
+
+serverSocket.bind(SERVER_ADDRESS)  # binding the server
+serverSocket.listen(10)  # listen to 10 clients connection.
+
+while True:
+    # Establish the connection
+    print('Ready to serve...')
+    connectionSocket, addr = serverSocket.accept() # obtaining established connection from the backlog queue.
+    try:
+        message = connectionSocket.recv(5000).decode() # decoding the message to bits.
+        filename = message.split()[1]
+        f = open(filename[1:])
+
+        # sending a basic message to our client, including HTTP Header :
+        outputdata = "HTTP/1.1 200 OK\r\n"
+        outputdata += "Content-Type: text/html; charset=utf-8\r\n"
+        outputdata += "\r\n"
+        outputdata += "<html><body>Hello World</body></html>\r\n\r\n"
+
+        for i in range(0, len(outputdata)): # can use also send all instead, right now for this project using this.
+            connectionSocket.send(outputdata[i].encode()) # encoding bits to real language.
+        connectionSocket.send("\r\n".encode()) # encoding bits to real language.
+
+        connectionSocket.close()
+    except IOError:
+        # Send response message for file not found
+        print("\n 404 Not Found")
+# Close client socket
+# Fill in start
+# Fill in end
+serverSocket.close()
+sys.exit()  # Terminate the program after sending the corresponding data
